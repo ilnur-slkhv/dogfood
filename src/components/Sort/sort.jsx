bec5ff2152d92945c0c11bc3aaf9d8f3800dedf1
ilnur-slkhv <ilnur.slkhv@gmail.com>
2023-01-03 23:02:59 +0300

    Сверстали сортировку

--- src/components/Sort/sort.jsx
+++ src/components/Sort/sort.jsx
@@ -1,7 +1,26 @@
+import classNames from "classnames";
 import "./styles.css";
 
-const Sort = () => {
-  return null;
+const Sort = ({ currentSort, tabs = [], onChangeSort }) => {
+  const handleClick = (e, tab) => {
+    e.preventDefault();
+    onChangeSort(tab.id);
+  };
+  return (
+    <div className="sort content__sort">
+      {tabs.map((tab) => (
+        <div
+          className={classNames("sort__link", {
+            sort__link_selected: currentSort === tab.id,
+          })}
+          key={tab.id}
+          id={tab.id}
+        >
+          <a onClick={(e) => handleClick(e, tab)}>{tab.title}</a>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Sort;
--- src/pages/CatalogPage/catalog-page.jsx
+++ src/pages/CatalogPage/catalog-page.jsx
@@ -4,11 +4,26 @@ import Sort from "../../components/Sort/sort";
 import Spinner from "../../components/Spinner/spinner";
 import { CardContext } from "../../context/cardContext";
 
+const tabs = [
+  {
+    id: "cheap",
+    title: "Сначала дешёвые",
+  },
+  {
+    id: "low",
+    title: "Сначала дорогие",
+  },
+  {
+    id: "sale",
+    title: "По скидке",
+  },
+];
+
 export const CatalogPage = ({ isLoading }) => {
   const { cards } = useContext(CardContext);
   return (
     <>
-      <Sort />
+      <Sort tabs={tabs} />
       <div className="content__cards">
         {isLoading ? <Spinner /> : <CardList cards={cards} />}
       </div>
